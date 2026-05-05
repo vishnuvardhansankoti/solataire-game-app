365aff6de85208c6be198526be8c5a7b6ea03981
vishnuvardhansankoti <shankotai@hotmail.com>
2026-05-04 21:05:06 -0500

    fix(build): import sveltekit from @sveltejs/kit/vite not vite-plugin-svelte

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import { sveltekit } from '@sveltejs/vite-plugin-svelte';
+import { sveltekit } from '@sveltejs/kit/vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
